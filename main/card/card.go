b746b8dcd87badbf54aa8375d2d9337129621ad3
Liam Brown <liambrownweb@gmail.com>
2020-12-09 18:20:44 -0700

    First commit

---
+++ main/card/card.go
@@ -0,0 +1,60 @@
+components {
+  id: "card_back"
+  component: "/main/card/card.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "card_suite"
+  component: "/main/card/card_back.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.5
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "card_symbol"
+  component: "/main/card/card_symbol.sprite"
+  position {
+    x: -240.0
+    y: 476.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "card"
+  component: "/main/card/card.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/globals.lua
@@ -0,0 +1,16 @@
+require "/main/utils"
+SUN = hash("SUN")
+MOON = hash("MOON")
+KEY = hash("KEY")
+NIGHTMARE = hash("NIGHTMARE")
+DOOR = hash("DOOR")
+OBSERVATORY = hash("OBSERVATORY")
+AQUARIUM = hash("AQUARIUM")
+GARDEN = hash("GARDEN")
+LIBRARY = hash("LIBRARY")
+SUITE = hash("SUITE")
+SYMBOL = hash("SYMBOL")
+actions = {
+	DRAW = hash("draw"),
+	DRAW_RESULT = hash("drawn card")
+}
